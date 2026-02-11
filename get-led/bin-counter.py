5a44823d282ecd63a44bb062fb3137b861d50fbe
SGFilms <stas.golovanov.07@mail.ru>
2026-02-11 15:15:26 +0300

    modified digital-switch and bin-counter

--- get-led/bin-counter.py
+++ get-led/bin-counter.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO, time as t
 GPIO.setmode(GPIO.BCM)
 
 leds = [24, 22, 23, 27, 17, 25, 12, 16]
+leds = leds[::-1]
 
 GPIO.setup(leds, GPIO.OUT)
 GPIO.output(leds, 0)
--- get-led/digital-switch.py
+++ get-led/digital-switch.py
@@ -8,12 +8,20 @@ GPIO.setup(led, GPIO.OUT)
 button = 13
 GPIO.setup(button, GPIO.IN)
 
-state = 0
+state = True
 
 while True:
-    if GPIO.input(button):
+    if GPIO.input(button) == 1:
         state = not state
         GPIO.output(led, state)
         t.sleep(0.2)
+
+        while GPIO.input(button) == 1:
+            t.sleep(0.01)
+        t.sleep(0.01)
+    
+
+
+        
 
 
