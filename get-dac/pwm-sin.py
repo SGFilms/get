c77ecf3a7ec0e5d3f972cdae3ec2ed267a9b83e2
SGFilms <stas.golovanov.07@mail.ru>
2026-02-25 14:16:58 +0300

    added 6

--- get-dac/pwm-sin.py
+++ get-dac/pwm-sin.py
@@ -1,14 +1,14 @@
 import pwm_dac, signal_generator as sg
 
 amplitude = 3
-signal_frequency = 10
-sampling_frequency = 1000
+signal_frequency = 1
+sampling_frequency = 20000
 
 dt = 1.0 / sampling_frequency
 t = 0.0
 
 if __name__ == '__main__':
-    dac = pwm_dac.PWM_DAC(12, 20000, 3.153, True)
+    dac = pwm_dac.PWM_DAC(12, 500, 3.153, True)
     try:
 
         while True:
