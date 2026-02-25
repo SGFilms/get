4a3874fae432f7dfe1448a047750b1b614a9bc37
SGFilms <stas.golovanov.07@mail.ru>
2026-02-25 14:38:35 +0300

    added working 6 and 7

---
+++ get-dac/mcp4725-sin.py
@@ -0,0 +1,22 @@
+import mcp4725_driver, signal_generator as sg
+
+amplitude = 3
+signal_frequency = 10
+sampling_frequency = 20000
+
+dt = 1.0 / sampling_frequency
+t = 0.0
+
+if __name__ == '__main__':
+    dac = mcp4725_driver.MCP4725(dynamic_range = 5.04, verbose = True)
+    try:
+
+        while True:
+            norm_amp = sg.get_sin_wave_amplitude(signal_frequency, t)
+            dac.set_voltage(norm_amp * amplitude)
+            sg.wait_for_sampling_period(sampling_frequency)
+            t += dt
+
+
+    finally:
+        dac.deinit()
